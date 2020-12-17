c8e61828227f37a50f9806b3ee0bcc7b95237b49
Fritz Geib <github-f@standland.org>
2020-12-16 22:04:02 -0500

    begin implementing CTRE drivetrain

--- src/main/java/frc/lightning/subsystems/CTREDriveTrain.java
+++ src/main/java/frc/lightning/subsystems/CTREDriveTrain.java
@@ -12,15 +12,18 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lightning.LightningConfig;
 import frc.lightning.util.RamseteGains;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 public class CTREDriveTrain extends SubsystemBase implements LightningDrivetrain {
   // private static final double CLOSE_LOOP_RAMP_RATE = 0.5;
@@ -30,38 +33,78 @@ public class CTREDriveTrain extends SubsystemBase implements LightningDrivetrain
   private final String name = "DRIVETRAIN";
 
   private BaseMotorController[] leftSlaves;
-  private TalonSRX leftMaster;
+  private BaseMotorController leftMaster;
   // private CANEncoder leftEncoder;
   // private CANPIDController leftPIDFController;
 
   private BaseMotorController[] rightSlaves;
-  private TalonSRX rightMaster;
+  private BaseMotorController rightMaster;
   // private CANEncoder rightEncoder;
   // private CANPIDController rightPIDFController;
 
-  public CTREDriveTrain(TalonSRX leftMaster, TalonSRX rightMaster, BaseMotorController[] leftSlaves,
-      BaseMotorController[] rightSlaves) {
+  private DifferentialDriveKinematics kinematics;
+
+  private DifferentialDriveOdometry odometry;
+
+  private SimpleMotorFeedforward feedforward;
+
+  private PIDController leftPIDController;
+
+  private PIDController rightPIDController;
+
+  private Pose2d pose = new Pose2d(0d, 0d, new Rotation2d());
+
+  private Pose2d poseOffset = null;
+
+  private RamseteGains gains;
+
+  public CTREDriveTrain(BaseMotorController leftMaster, BaseMotorController rightMaster, BaseMotorController[] leftSlaves,
+      BaseMotorController[] rightSlaves, double trackWidth, RamseteGains gains) {
     setName(name);
     this.leftMaster = leftMaster;
     this.rightMaster = rightMaster;
     this.leftSlaves = leftSlaves;
     this.rightSlaves = rightSlaves;
 
+    this.gains = gains;
+
     configureFollows();
 
     brake();
     init();
+
+    kinematics = new DifferentialDriveKinematics(trackWidth);
+
+    odometry = new DifferentialDriveOdometry(getHeading(), pose);
+    
+
+    feedforward = new SimpleMotorFeedforward(gains.getkS(), gains.getkV(), gains.getkA());
+
+    leftPIDController = new PIDController(gains.getLeft_kP(), gains.getLeft_kI(), gains.getLeft_kD());
+        
+    rightPIDController = new PIDController(gains.getRight_kP(), gains.getRight_kI(), gains.getRight_kD());
+  }
+
+  @Override
+  public void periodic(){
+    super.periodic();
+
+    SmartDashboard.putNumber("RightMasterHeat", rightMaster.getTemperature());
+    SmartDashboard.putNumber("LeftMasterHeat", leftMaster.getTemperature());
+
+    pose = odometry.update(getHeading(), getLeftDistance(), getRightDistance());
+
   }
 
   public void init() {
     this.resetDistance();
   }
 
-  protected TalonSRX getLeftMaster() {
+  protected BaseMotorController getLeftMaster() {
     return leftMaster;
   }
 
-  protected TalonSRX getRightMaster() {
+  protected BaseMotorController getRightMaster() {
     return rightMaster;
   }
 
@@ -85,7 +128,7 @@ public class CTREDriveTrain extends SubsystemBase implements LightningDrivetrain
     }
   }
 
-  protected void withEachSlaveMotor(BiConsumer<BaseMotorController, TalonSRX> op) {
+  protected void withEachSlaveMotor(BiConsumer<BaseMotorController, BaseMotorController> op) {
     for (var m : leftSlaves)
       op.accept(m, leftMaster);
     for (var m : rightSlaves)
@@ -171,50 +214,46 @@ public class CTREDriveTrain extends SubsystemBase implements LightningDrivetrain
 
   @Override
   public DifferentialDriveKinematics getKinematics() {
-    // TODO Auto-generated method stub
-    return null;
+    return kinematics;
   }
 
   @Override
   public Pose2d getPose() {
-    // TODO Auto-generated method stub
-    return null;
+    return pose;
   }
 
   @Override
   public DifferentialDriveWheelSpeeds getSpeeds() {
-    // TODO Auto-generated method stub
-    return null;
+    return new DifferentialDriveWheelSpeeds(getLeftVelocity(), getRightVelocity());
   }
 
   @Override
   public void setOutput(double leftVolts, double rightVolts) {
-    // TODO Auto-generated method stub
+    SmartDashboard.putNumber("RequestedLeftVolts", leftVolts);
+    SmartDashboard.putNumber("RequestedRightVolts", rightVolts);
 
+    leftMaster.set(ControlMode.Velocity, leftVolts);
+    rightMaster.set(ControlMode.Velocity, rightVolts);
   }
 
   @Override
   public PIDController getLeftPidController() {
-    // TODO Auto-generated method stub
-    return null;
+    return leftPIDController;
   }
 
   @Override
   public PIDController getRightPidController() {
-    // TODO Auto-generated method stub
-    return null;
+    return rightPIDController;
   }
 
   @Override
   public double getRightVolts() {
-    // TODO Auto-generated method stub
-    return 0;
+    return rightMaster.getMotorOutputVoltage() * LightningConfig.VOLT_LIMIT;
   }
 
   @Override
   public double getLeftVolts() {
-    // TODO Auto-generated method stub
-    return 0;
+    return leftMaster.getMotorOutputVoltage() * LightningConfig.VOLT_LIMIT;
   }
 
   @Override
@@ -237,13 +276,11 @@ public class CTREDriveTrain extends SubsystemBase implements LightningDrivetrain
 
   @Override
   public Pose2d getRelativePose() {
-    // TODO Auto-generated method stub
-    return null;
+    return pose.relativeTo(poseOffset);
   }
 
   @Override
   public void setRelativePose() {
-    // TODO Auto-generated method stub
-
+    poseOffset = pose;
   }
 }
