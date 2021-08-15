94dac036e3f6b958caeb50183c18125aa911541e
edurso <eric@casadurso.com>
2021-08-14 20:25:49 -0400

    PDEV-100 add sample - works on howitzer

---
+++ samples/swerve-simple/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,54 @@
+package frc.robot;
+
+import com.lightningrobotics.common.LightningContainer;
+import com.lightningrobotics.common.command.drivetrain.swerve.SwerveDriveCommand;
+import com.lightningrobotics.common.subsystem.core.LightningIMU;
+import com.lightningrobotics.common.subsystem.drivetrain.LightningDrivetrain;
+import com.lightningrobotics.common.subsystem.drivetrain.swerve.SwerveDrivetrain;
+
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.subsystems.Drivetrain;
+
+public class RobotContainer extends LightningContainer {
+
+	private static final XboxController driver = new XboxController(0);
+
+	private static final LightningIMU imu = LightningIMU.pigeon(19);
+
+	private static final SwerveDrivetrain drivetrain = new Drivetrain();
+
+	@Override
+	protected void configureButtonBindings() { }
+
+	@Override
+	protected void configureSystemTests() { }
+
+	@Override
+	protected void configureDefaultCommands() {
+		drivetrain.setDefaultCommand(new SwerveDriveCommand(drivetrain, imu, driver, true));
+	}
+
+	@Override
+	protected void releaseDefaultCommands() { }
+
+	@Override
+	protected void initializeDashboardCommands() { }
+
+	@Override
+	protected void configureAutonomousPaths() { }
+
+	@Override
+	protected void configureAutonomousCommands() { }
+
+	@Override
+	protected void configureFaultCodes() { }
+
+	@Override
+	protected void configureFaultMonitors() { }
+
+	@Override
+	public LightningDrivetrain getDrivetrain() {
+		return drivetrain;
+	}
+
+}
