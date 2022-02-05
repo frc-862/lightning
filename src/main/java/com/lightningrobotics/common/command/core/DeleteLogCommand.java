f624712c24d7f38591810acf13b1f89c0a55eb7f
Ziming Huang <Ziming412@gmail.com>
2022-02-04 19:56:44 -0500

    Comments and javadocs

--- src/main/java/com/lightningrobotics/common/command/core/DeleteLogCommand.java
+++ src/main/java/com/lightningrobotics/common/command/core/DeleteLogCommand.java
@@ -10,30 +10,37 @@ import java.util.Arrays;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/**
+ * Deletes RoboRio log files
+ */
 public class DeleteLogCommand extends CommandBase {
 
   private int filesToKeep;
 
   private boolean isDone;
 
-  File logFolder = new File("/home/lvuser/log");
-  File logFiles[] = logFolder.listFiles(); // TODO: add method of finding size of directory
+  // Default log directory in Roborio
+  File logFolder = new File("/home/lvuser/log"); 
+  File logFiles[] = logFolder.listFiles();
   
+  /**
+   * Commmand that deletes a certain number of log files, starting from the oldest
+   * @param filesToKeep Number of files to keep in the RoboRio
+   */
   public DeleteLogCommand(int filesToKeep) {
     this.filesToKeep = filesToKeep;
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {}
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     SmartDashboard.putNumber("logFileCount", getLogAmount());
     Arrays.sort(logFiles);
 
     if (logFiles.length > filesToKeep) {
+      // Calculating number of files to delete
       int overcount = logFiles.length - filesToKeep;
       
       for (int i = 0; i < overcount; i++) {
@@ -42,6 +49,7 @@ public class DeleteLogCommand extends CommandBase {
 
       logFiles = logFolder.listFiles();
 
+      // Renaming log files to "DeleteLogCommands-<i + 1>05-dl.log"
       for (int i = 0; i < filesToKeep; i++) {
         logFiles[i].renameTo(new File(String.format("/home/lvuser/log/%s-%05d-dl.log", getClass().getSimpleName(), i + 1)));
       }
